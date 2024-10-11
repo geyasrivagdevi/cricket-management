aabed18eca79bced9fe9a392d6a4b31a5dc67fcb
geyasrivagdevi <methuku1499@gmail.com>
2024-10-11 18:56:33 +1300

    CMS

--- server.js
+++ server.js
@@ -791,7 +791,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3306;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
